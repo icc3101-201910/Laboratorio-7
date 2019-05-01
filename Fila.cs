c5fa7969284fb6847e8c846bf5d7bf514abb331f
Nicolás Gómez <nicolas@mtblc.co>
2019-05-01 17:49:32 -0400

    :sparkles: incremento 1

---
+++ Fila.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace laboratorio7
+{
+    public class Fila
+    {
+        public int regionID;
+        public string region;
+        public int comunaID;
+        public string comuna;
+        public int administrationID;
+        public string administration;
+        public double psu;
+        public double nem;
+        public int numeroDatos;
+
+        public Fila(string filaCsv)
+        {
+            string[] partes = filaCsv.Split(',');
+            regionID = Convert.ToInt32(partes[0]);
+            region = partes[1];
+            comunaID = Convert.ToInt32(partes[2]);
+            comuna = partes[3];
+            administrationID = Convert.ToInt32(partes[4]);
+            administration = partes[5];
+            // Los siguientes datos pueden ser vacíos, por eso la verificación
+            if (partes[6] != "") psu = Convert.ToDouble(partes[6]);
+            if (partes[7] != "") nem = Convert.ToDouble(partes[7]);
+            numeroDatos = (int) Convert.ToDouble(partes[8]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace laboratorio7
 {
@@ -6,7 +9,23 @@ namespace laboratorio7
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // -- Lectura de datos --
+            Console.WriteLine("Importando datos...");
+
+            StreamReader reader = new StreamReader("data.csv");
+            reader.ReadLine(); // La primera linea es el encabezado
+            string row = reader.ReadLine();
+
+            List<Fila> datos = new List<Fila>();
+            
+            while (row != null)
+            {
+                datos.Add(new Fila(row));
+                row = reader.ReadLine();
+            }
+            reader.Close();
+
+            Console.WriteLine($"Se han importado {datos.Count} datos");
         }
     }
 }
